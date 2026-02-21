8c3f4e3d47a1c738cab36b9929e4a70a26de7702
Mahmood Khan <3mahmoodkhan@gmail.com>
2026-02-21 12:52:09 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'naswar-dealer-v12'; // IMPORTANT: Increment this (v4, v5...) every time you update code!
+const CACHE_NAME = 'naswar-dealer-v2'; // IMPORTANT: Increment this (v4, v5...) every time you update code!
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
